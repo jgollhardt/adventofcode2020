c6601579460b4d4b0f9e623da87a8c1606aee52b
Jared Gollhardt <jgollhardt@gmail.com>
2020-12-01 21:14:55 -0800

    Add solutions

--- day2/puzzle.js
+++ day2/puzzle.js
@@ -1,26 +1,27 @@
 import fs from 'fs';
 import { fetchInput } from '../utils/fetch.js';
 
+// 434
 const puzzle1 = (lines) => {
   const valids = lines.filter(line => {
     const [min, max, letter, password] = line;
     let count = 0;
-    for (let i = 0; i < password.length; i++) {
-      const char = password[i];
+    for (const char of password) {
       if (char === letter) count += 1;
     }
 
     return min <= count && count <= max;
-  })
+  });
 
   return valids.length;
 }
 
+// 509
 const puzzle2 = (lines) => {
   const valids = lines.filter(line => {
     const [min, max, letter, password] = line;
-    return password[min - 1] === letter ^ password[max - 1] === letter
-  })
+    return password[min - 1] === letter ^ password[max - 1] === letter;
+  });
 
   return valids.length;
 }
