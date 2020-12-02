a68bd6a06292a54d67bcd60105218936c6628b2f
Jared Gollhardt <jgollhardt@gmail.com>
2020-12-01 21:11:27 -0800

    day2 solution

--- day2/puzzle.js
+++ day2/puzzle.js
@@ -2,16 +2,38 @@ import fs from 'fs';
 import { fetchInput } from '../utils/fetch.js';
 
 const puzzle1 = (lines) => {
-  return lines;
+  const valids = lines.filter(line => {
+    const [min, max, letter, password] = line;
+    let count = 0;
+    for (let i = 0; i < password.length; i++) {
+      const char = password[i];
+      if (char === letter) count += 1;
+    }
+
+    return min <= count && count <= max;
+  })
+
+  return valids.length;
 }
 
 const puzzle2 = (lines) => {
+  const valids = lines.filter(line => {
+    const [min, max, letter, password] = line;
+    return password[min - 1] === letter ^ password[max - 1] === letter
+  })
+
+  return valids.length;
 }
 
 await fetchInput();
 
 // const data = fs.readFileSync('test_input1.txt', 'utf-8')
 const data = fs.readFileSync('input.txt', 'utf-8');
-const lines = data.trim().split('\n') // .map(line => parseInt(line));
-console.log(puzzle1(lines));
-// console.log(puzzle2(lines));
+const lines = data.trim().split('\n')
+  .map(line => {
+    const parts = line.split(" ");
+    return [...parts[0].split('-').map(part => parseInt(part)), parts[1][0], parts[2]];
+  });
+// .map(line => parseInt(line));
+// console.log(puzzle1(lines));
+console.log(puzzle2(lines));
