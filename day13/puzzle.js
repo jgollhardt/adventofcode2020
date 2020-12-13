000c8f78290c06c3a3194c0e4c25251ae5729e2b
Jared Gollhardt <jgollhardt@gmail.com>
2020-12-12 23:09:22 -0800

    Day 13 solutions

--- day13/puzzle.js
+++ day13/puzzle.js
@@ -2,15 +2,60 @@ import _ from 'lodash';
 import fs from 'fs';
 import { fetchInput } from '../utils/fetch.js';
 
+// 2406
 const puzzle1 = (lines) => {
-  return lines;
+  let [earliest, buses] = lines;
+  earliest = parseInt(earliest);
+  buses = buses
+    .split(',')
+    .filter((bus) => bus !== 'x')
+    .map((bus) => parseInt(bus));
+
+  let starting = buses.map((bus) => {
+    let tmp = bus;
+    while (tmp < earliest) tmp += bus;
+    return { bus, tmp };
+  });
+
+  const min = _.minBy(starting, 'tmp');
+
+  return (min.tmp - earliest) * min.bus;
 };
 
-const puzzle2 = (lines) => {};
+// 225850756401039
+const puzzle2 = (lines) => {
+  let [, buses] = lines;
+  buses = _.transform(buses.split(','), (result, bus, index) => {
+    if (bus !== 'x') {
+      result.push({
+        bus: parseInt(bus),
+        index,
+      });
+    }
+  });
+
+  let alephBus = buses[0];
+  for (let i = 1; i < buses.length; i++) {
+    const { bus, index } = buses[i];
+    let x = alephBus.index;
+    let newBus;
+    while (!newBus) {
+      if ((x + index) % bus === 0) {
+        newBus = { bus: alephBus.bus * bus, index: x };
+        break;
+      }
+      x += alephBus.bus;
+    }
+
+    alephBus = newBus;
+  }
+
+  return alephBus.index;
+};
 
 await fetchInput();
 
-// const data = fs.readFileSync('test_input1.txt', 'utf-8')
+// const data = fs.readFileSync('test_input1.txt', 'utf-8');
 const data = fs.readFileSync('input.txt', 'utf-8');
 const lines = data.trim().split('\n');
 console.log(puzzle1(lines));
