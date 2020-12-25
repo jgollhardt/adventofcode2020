426cff972184d1422212a381e78034b666f0d0e8
Jared Gollhardt <jgollhardt@gmail.com>
2020-12-24 21:17:41 -0800

    Day 25 solutions

--- day25/puzzle.js
+++ day25/puzzle.js
@@ -2,15 +2,34 @@ import _ from 'lodash';
 import fs from 'fs';
 import { fetchInput } from '../utils/fetch.js';
 
+// 6198540
 const puzzle1 = (lines) => {
-  return lines;
+  const [pub1, pub2] = lines.map((line) => parseInt(line));
+
+  let val = 1;
+  let sub = 7;
+  let round = 0;
+  while (val !== pub1) {
+    val *= sub;
+    val %= 20201227;
+    round += 1;
+  }
+
+  val = 1;
+  sub = pub2;
+  _.range(round).forEach((i) => {
+    val *= sub;
+    val %= 20201227;
+  });
+
+  return val;
 };
 
 const puzzle2 = (lines) => {};
 
 await fetchInput();
 
-// const data = fs.readFileSync('test_input1.txt', 'utf-8')
+// const data = fs.readFileSync('test_input1.txt', 'utf-8');
 const data = fs.readFileSync('input.txt', 'utf-8');
 const lines = data.trim().split('\n');
 console.log(puzzle1(lines));
